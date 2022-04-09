1452d073a60c74b1b4a4dc4e94b89bcd813d9d4f
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-04-09 12:17:54 -0300

    09-04-22

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade detectada foi ${vel}Km/h.`)
+if (vel > 60) { //Condição SIMPLES, onde não existe um ação para a resolução FALSE
+  console.log(`${vel}Km/h ultrapassa a velocidade permitida. Você foi multado!`)
+}
+console.log('Dirija sempre usando cinto de segurança.')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var country = 'EUA'
+if (country == 'Brasil') {
+  console.log('Você é Brasileiro.')
+}
+else {
+  console.log('Você é estrangeiro.')
+}
