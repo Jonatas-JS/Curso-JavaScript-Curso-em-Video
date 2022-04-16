8d25a9bc5d91f3c34699a8863f77d0acb16fc41c
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-04-16 00:10:42 -0300

    descobrindo-o-dia-da-semana

---
+++ aula12/ex011.js
@@ -0,0 +1,22 @@
+var idade = 22
+if (idade < 16) {
+  console.log('Não vota')
+}else if (idade < 18 || idade > 65) {
+    console.log('Voto opcional')
+  }else { // se não se enquadrou nas oções anteriores ele certamente tem >= 18
+    console.log('Voto obrigatório')
+  }
+
+  /* OURTA FORMA, MAIS LONGA
+  var idade = 67
+  if (idade < 16) {
+    console.log('Não vota')
+  }else {
+    if (idade <= 16 && idade < 18 || idade > 65) {
+      console.log('Voto opcional')
+    }else {
+      if (idade >= 18) {
+        console.log('Voto obrigatório')
+      }
+    }
+  } */
