03f6d60488adb374755ebde14ae458c5f7cb90ba
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-04-18 17:08:19 -0300

    correções-do-exercício-hora-do-dia

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -8,14 +8,15 @@ function carregar() {
 
   msg.innerHTML = `<p>Agora são ${hora}:${minutos}h.</p>`
 
-  if (hora >= 0 && hora < 12) {
-    img.innerHTML = '<img src="/images/foto-manha.png">'
+  if (hora >= 0 && hora < 12) { //Bom dia!
+    img.src = 'images/foto-manha.png' //A VAR IMG está contendo a estrutura HTML(<img src"">),
+                              // o que estou fazendo é acessar os valores do atributo SRC que está em IMG.
     document.body.style.background = '#E2CD9F'
-  } else if (hora >= 12 && hora < 18) {
-    img.innerHTML = '<img src="/images/foto-tarde.png">'
+  } else if (hora >= 12 && hora < 18) { //Boa tarde!
+    img.src = 'images/foto-tarde.png'
     document.body.style.background = '#B9846F'
-  } else {
-    img.innerHTML = '<img src="/images/foto-noite.png">'
+  } else {  //Boa noite!
+    img.src = 'images/foto-noite.png'
     document.body.style.background = '#515154'
   }
 }
