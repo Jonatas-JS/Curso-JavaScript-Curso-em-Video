2e71c9642caaaa594d72ee3bb74afa9edba2425b
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-04-22 17:10:50 -0300

    estrutura-while-e-dowhile

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,12 +1,14 @@
-// ESTRUTURA WHILE
 var cont = 1 // variável que vai contar
+
+// ESTRUTURA WHILE
+/*
 while (cont <= 6) {
   console.log(`Passo ${cont}`)
   cont++ //auto incremente
-}
+} */
 
 // ESTRUTURA DOWHILE
 do { // faça
-  console.log(`Passo ${cont}`)
+  console.log(`Comer pedaço ${cont}`)
   cont++
 } while (cont <= 6) // enquanto
