743af2e6a73fc25ea8c9756b5540b259a0d5bd63
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-04-27 11:29:47 -0300

    estruturas de repetição while, dowhile e for

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,14 +1,13 @@
 var cont = 1 // variável que vai contar
 
 // ESTRUTURA WHILE
-/*
 while (cont <= 6) {
   console.log(`Passo ${cont}`)
   cont++ //auto incremente
-} */
+}
 
 // ESTRUTURA DOWHILE
 do { // faça
   console.log(`Comer pedaço ${cont}`)
   cont++
-} while (cont <= 6) // enquanto
+} while (cont <= 8) // enquanto
---
+++ aula14/ambiente.js
@@ -0,0 +1,22 @@
+
+// Fazendo usando o método WHILE
+/* var cont = 1
+while (cont <= 5) {
+  console.log(cont)
+  c++
+ }*/
+console.log('Vamos começar')
+ //Usando o FOR você tem o mesmo resultado e uma lógica parecida com o exemplo acima.
+ for (var cont = 1; cont <= 5; cont++) {
+//O FOR terá três partes.
+//INICIALIZAÇÃO ; CONDIÇÃO ; INCREMENTE
+
+//Inicialização: tudo que precisará ser criado para ser usado na condição.
+//Condição: teste lógico.
+//Incremente: depois de finalizar o que tem dentro bloco, quando ele voltar para o início do FOR fará o incremente.
+
+console.log(cont)
+//Obs.: depois do console.log(cont) que ele volta para o FOR ele vai fazer apenas o incremente primeiro e o condção
+ }
+ console.log('Terminou')
+ //Todo o processo de execução pode ser acompanhado pelo Debug(atalho F5) e Debug Console(próximo ao Terminal)
